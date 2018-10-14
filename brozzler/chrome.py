635d3d780f412d1ce37dd1ded5b4a52b69857b9f
Babibubebon <babibubebon@babibubebo.org>
2018-10-14 10:52:44 +0900

    add chrome_args to enable flash plugin

--- brozzler/chrome.py
+++ brozzler/chrome.py
@@ -171,7 +171,10 @@ class Chrome:
                 '--disable-first-run-ui', '--no-first-run',
                 '--homepage=about:blank', '--disable-direct-npapi-requests',
                 '--disable-web-security', '--disable-notifications',
-                '--disable-extensions', '--disable-save-password-bubble']
+                '--disable-extensions', '--disable-save-password-bubble',
+                '--flag-switches-begin',
+                '--enable-features=RunAllFlashInAllowMode', '--disable-features=PreferHtmlOverPlugins',
+                '--flag-switches-end']
         if self.ignore_cert_errors:
             chrome_args.append('--ignore-certificate-errors')
         if proxy:
